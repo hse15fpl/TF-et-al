52c6b5ea176112f5cea497603bf1f1f6f5b63713
Pythonimous <Pythonimous@users.noreply.github.com>
2018-11-21 20:43:23 +0300

    Задание 7 калькулятор
    Файл flask_calculator основной, calculator - вспомогательный

---
+++ Николаев Кирилл/Задание 7/calculator.py
@@ -0,0 +1,100 @@
+# Строковый калькулятор
+
+import re, sys, math
+
+def calculate(expression):
+
+	parsed = []
+
+	def parser(x):
+		if len(x) > 0:
+			if x[0] in "(+-*/^v).":
+				parsed.append(x[0])
+				parser(x[1:])
+			elif x[0].isdigit:
+				parsed.append(re.findall('\d+', x)[0])
+				x = x.replace(re.findall('\d+', x)[0], '', 1)
+				parser(x)
+
+
+
+	def calc(x):
+
+		if x[0] == '(' and x[-1] == ')':
+			x = x[1:-1]
+
+		if '.' in x:
+			for ind in range(len(x)):
+				if x[ind] == '.':
+					x[ind-1], x[ind], x[ind+1] = 'x', 'x', ''.join([x[ind - 1], x[ind], x[ind + 1]])
+			x = [i for i in x if i != 'x']
+
+		if '-' in x:
+			for ind in range(len(x)):
+				if x[ind] == '-':
+					if ind == 0:
+						x[ind], x[ind + 1] = 'x', str(0 - float(x[ind + 1]))
+					elif not x[ind - 1].isdigit():
+						x[ind], x[ind + 1] = 'x', str(0 - float(x[ind + 1]))
+			x = [i for i in x if i != 'x']
+
+		if '(' in x:
+			flag = False
+			opening = []
+			ind = 0
+			length = len(x)
+			while ind < length:
+				if x[ind] == '(':
+					flag = True
+					opening.append(ind)
+					ind += 1
+				elif x[ind] == ')' and flag == True:
+					newexpr = x[opening[-1]:ind+1]
+					x = x[:opening[-1]] + [str(calc(newexpr))] + x[ind + 1:]
+					ind = opening[-1] + 1
+					opening = opening[:-1]
+					length = len(x)
+				else:
+					ind += 1
+		if '^' in x:
+			for ind in range(len(x)):
+				if x[ind] == '^':
+					x[ind-1], x[ind], x[ind+1] = 'x', 'x', str(float(x[ind - 1]) ** float(x[ind + 1]))
+			x = [i for i in x if i != 'x']
+
+		if 'v' in x:
+			for ind in range(len(x)):
+				if x[ind] == 'v':
+					if float(x[ind+1]) < 0:
+						print('Обнаружен корень из отрицательного числа ('+str(float(x[ind+1]))+').')
+						print('Сначала преобразуем в положительное ('+str(0 - float(x[ind+1]))+').')
+						x[ind+1] = str(0-float(x[ind+1]))
+					x[ind], x[ind+1] = 'x', str(math.sqrt(float(x[ind+1])))
+			x = [i for i in x if i != 'x']
+
+		if '*' in x or '/' in x:
+			for ind in range(len(x)):
+				if x[ind] == '*':
+					x[ind - 1],x[ind],x[ind+1] = 'x', 'x', str(float(x[ind - 1]) * float(x[ind + 1]))
+				elif x[ind] == '/':
+					x[ind-1], x[ind], x[ind+1] = 'x', 'x', str(float(x[ind - 1]) / float(x[ind + 1]))
+			x = [i for i in x if i != 'x']
+
+		if '-' in x or '+' in x:
+			for ind in range(len(x)):
+				if x[ind] == '-':
+					x[ind-1], x[ind], x[ind+1] = 'x', 'x', str(float(x[ind - 1]) - float(x[ind + 1]))
+				elif x[ind] == '+':
+					x[ind-1], x[ind], x[ind+1] = 'x', 'x', str(float(x[ind - 1]) + float(x[ind + 1]))
+			x = [i for i in x if i != 'x']
+
+		return x[0]
+
+	try:           # Универсальная проверка на дурака
+		parser(expression)
+		solution = calc(parsed)
+		outputstr = 'Ответ: ' + str(solution)
+	except:
+		outputstr = 'Неверное выражение, попробуйте ещё.'
+		
+	return outputstr
---
+++ Николаев Кирилл/Задание 7/flask_calculator.py
@@ -0,0 +1,59 @@
+from flask import Flask, request
+import calculator
+
+app = Flask(__name__)
+
+template1 = """
+<!DOCTYPE html>
+<html><head>
+<title>СУПЕРМЕГАКАЛЬКУЛЯТОР™!</title>
+</head>
+<body>
+	<p>Добро пожаловать в СУПЕРМЕГАКАЛЬКУЛЯТОР™ (СМК)!</p>
+	<p>СМК умеет парсить строку, в том числе скобки :)</p>
+	<p>Возможные действия: +, -, *, /, ^ (возведение в степень), v (извлечение квадратного корня). Для десятичных, пожалуйста, используйте точку. (.)</p>
+	<p>Пы.Сы. При извлечении корня из отрицательных чисел, они сначала преобразуются в положительные.</p>
+	<form name="test" method="get" action="form"
+	<p><b>Введите выражение:</b><br>
+		<input name="name1" type="text" size="40">
+	</p>
+	<input type="submit" value="Submit">
+</form>
+<h1>Ответ:</h1>
+<p>%s</p>
+</body></html>
+"""
+
+template2 = """
+<!DOCTYPE html>
+<html><head>
+<title>СУПЕРМЕГАКАЛЬКУЛЯТОР™!</title>
+</head>
+<body>
+	<p>Добро пожаловать в СУМЕРМЕГАКАЛЬКУЛЯТОР™ (СМК)!</p>
+	<p>СМК умеет парсить строку, в том числе скобки :)</p>
+	<p>Возможные действия: +, -, *, /, ^ (возведение в степень), v (извлечение квадратного корня). Для десятичных, пожалуйста, используйте точку. (.)</p>
+	<p>Пы.Сы. При извлечении корня из отрицательных чисел, они сначала преобразуются в положительные.</p>
+	<form name="test" method="get" action="form"
+	<p><b>Введите выражение:</b><br>
+		<input name="name1" type="text" size="40">
+	</p>
+	<input type="submit" value="Submit">
+</form>
+<h1>Ответ для выражения {0}:</h1>
+<p>{1}</p>
+</body></html>
+"""
+
+@app.route('/')
+def hw():
+	return template1 %'Пока ответа нет, введите выражение!'
+
+@app.route('/form', methods=['GET'])
+def form():
+	n = request.args.get('name1')
+	answer = calculator.calculate(n)
+	return template2.format(n, answer)
+	
+if __name__ == "__main__":
+	app.run(port=8081, debug=True)
